3085e5ae17ea025d02221e5656e0b79fe1b38923
caseybreen <caseyfbreen@gmail.com>
2023-06-26 11:03:01 -0700

    update figures

---
+++ code/helpers.R
@@ -0,0 +1,8 @@
+## Source .Rmd 
+
+source_rmd = function(file, ...) {
+  tmp_file = tempfile(fileext=".R")
+  on.exit(unlink(tmp_file), add = TRUE)
+  knitr::purl(file, output=tmp_file, quiet = T)
+  source(file = tmp_file, ...)
+} 
